72eb2e50eddc591387e3d74e0f79cf9b5c516eb2
sunwei <me@sunwei.xyz>
2023-09-21 19:49:15 +0800

    switch engine to twopi when string over 400KB

--- src/graphviz/viz.ts
+++ src/graphviz/viz.ts
@@ -14,5 +14,17 @@ export function renderElement(dotSrc: string): Promise<Rendering> {
     const renderOptions = {
         engine: "dot"
     };
+    const isOver400KB: boolean = isStringOver400KB(dotSrc);
+    if (isOver400KB) {
+        renderOptions.engine = "twopi"
+    }
+
     return  viz.renderSVGElement(dotSrc, renderOptions).catch(catcher);
+}
+
+function isStringOver400KB(str: string): boolean {
+    const bytes = new TextEncoder().encode(str);
+    const kb = bytes.length / 1024;
+
+    return kb > 400;
 }
